ebf6053755cf8233c29a1d049d43a724e65f2aea
Alisha Singh <singhalisha1008@gmail.com>
2026-07-24 14:47:38 +0530

    initial commit

--- Lab1/fun.js
+++ Lab1/fun.js
@@ -10,3 +10,6 @@ console.log(toWords(5));
 console.log(toWords(0));
 console.log(toWords(9));
 
+//frontend clask,angular,react,vue
+//common html,css(bootstrap),event,tailwind
+//backend nodejs,spring boot,django,php,fastapi
